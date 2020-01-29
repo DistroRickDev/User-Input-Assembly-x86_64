a66b21c68dd2c704a4328b9d91367b541406e427
RetroRick <ricardrao.rbo@gmail.com>
2020-01-29 22:19:08 +0000

    first commit

---
+++ main.asm
@@ -0,0 +1,22 @@
+section .data
+    text db 0
+
+section .text
+    global _start
+
+_start:
+    mov rax, 0 ;sys_read
+    mov rdi, 0 ; default input 
+    mov rsi, text 
+    mov rdx, 13 
+    syscall
+
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, text
+    mov rdx, 13
+    syscall
+
+    mov rax, 60
+	mov rdi, 0
+	syscall    
